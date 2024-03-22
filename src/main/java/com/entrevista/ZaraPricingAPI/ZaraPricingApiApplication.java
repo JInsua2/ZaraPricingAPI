648a0b4c241e134aa8e67403c3bf475ce74b2e71
jinsua2 <jinsua2@gmail.com>
2024-03-22 22:16:46 +0100

    chore: set up basic project structure

---
+++ src/main/java/com/entrevista/ZaraPricingAPI/ZaraPricingApiApplication.java
@@ -0,0 +1,13 @@
+package com.entrevista.ZaraPricingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaraPricingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaraPricingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entrevista/ZaraPricingAPI/ZaraPricingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entrevista.ZaraPricingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaraPricingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
